d4c364d3163e3216e32dddfe31f2821e5b209363
Zac Brydon <zacbrydon@gmail.com>
2022-08-12 21:43:54 +1000

    Update Colour

--- arcade-games/Game 3/program.cpp
+++ arcade-games/Game 3/program.cpp
@@ -5,7 +5,7 @@ int main()
 	open_window("Example Game",600,400);
 	while(!quit_requested() && !key_typed(ESCAPE_KEY))
 	{
-		clear_screen(COLOR_BLACK);
+		clear_screen(COLOR_DEEP_PINK);
 		refresh_screen(60);
 		process_events();
         // Update
