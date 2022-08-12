d7c8ad9582d7c61a307001345b0c7d9dd57367e0
Zac Brydon <zacbrydon@gmail.com>
2022-08-12 23:51:10 +1000

    losing hope

--- arcade-games/Game-Three/program.cpp
+++ arcade-games/Game-Three/program.cpp
@@ -5,7 +5,7 @@ int main()
 	open_window("Example Game",600,400);
 	while(!quit_requested() && !key_typed(ESCAPE_KEY))
 	{
-		clear_screen(COLOR_ALICE_BLUE);
+		clear_screen(COLOR_DARK_OLIVE_GREEN);
 		refresh_screen(60);
 		process_events();
         // Update
