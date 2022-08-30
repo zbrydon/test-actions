5f1d61eb658b66613d288c38a312bde59bb00794
Zac Brydon <zacbrydon@gmail.com>
2022-08-31 04:30:39 +1000

    c_change_2

--- arcade-games/test-game/program.cpp
+++ arcade-games/test-game/program.cpp
@@ -5,7 +5,7 @@ int main()
 	open_window("TEST Game",600,400);
 	while(!quit_requested() && !key_typed(ESCAPE_KEY))
 	{
-		clear_screen(COLOR_BLANCHED_ALMOND);
+		clear_screen(COLOR_DARK_BLUE);
 		refresh_screen(60);
 		process_events();
         // Update
